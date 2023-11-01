9ad9fa0f4f06efd3661b30d0b81a37b25da2b215
EliSchillinger <145599100+EliSchillinger@users.noreply.github.com>
2023-10-31 18:07:20 -0700

    Add files via upload

--- Tree.java
+++ Tree.java
@@ -1,3 +1,55 @@
-public class Tree{
-    
+
+public class Tree {
+	public static class Node 
+	{
+		int numVal;
+		char letterVal;
+		Node leftChild;
+		Node rightChild;
+		
+		public Node(int num, char letter)
+		{
+			numVal = num;
+			letterVal = letter;
+			leftChild = null;
+			rightChild = null;
+		}
+		
+		public void connect(Node left, Node right)
+		{
+			leftChild = left;
+			rightChild = right;
+		}
+	}
+	
+	Node root;
+	
+	public Tree()
+	{	
+		root = null;
+	}
+	
+	public void setRoot(Node r)
+	{
+		root = r;
+	}
+	
+	public char traverse(String code, Node node)
+	{
+		if(code.length() <= 0)
+		{
+			return node.letterVal;
+		}
+		else
+		{
+			if(code.charAt(0) == 0)
+			{
+				return traverse(code.substring(1), node.leftChild);
+			}
+			else
+			{
+				return traverse(code.substring(1), node.rightChild);
+			}
+		}
+	}
 }
